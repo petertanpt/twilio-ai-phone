1ae08da1cb00602cd877eb5c39915789bdd2c209
petertanpt <petertanpt@yahoo.co.uk>
2025-06-07 01:46:13 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const { twiml } = require('twilio');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// 兼容 GET 请求，防止 Twilio GET /voice 时直接挂断
+app.get('/voice', (req, res) => {
+  res.send('✅ AI 电话服务在线，请使用 POST 请求以接听电话。');
+});
+
+// 正常 POST 路由供 Twilio 使用
 app.post('/voice', (req, res) => {
   const response = new twiml.VoiceResponse();
 
